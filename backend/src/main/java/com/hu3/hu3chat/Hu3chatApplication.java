bf2ad6700dc8d5b5f15c0dbedea46b3a40ccf075
TropicalCrow <mao97343@gmail.com>
2021-02-04 08:59:40 -0300

    Projeto criado

---
+++ backend/src/main/java/com/hu3/hu3chat/Hu3chatApplication.java
@@ -0,0 +1,13 @@
+package com.hu3.hu3chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hu3chatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hu3chatApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hu3/hu3chat/Hu3chatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hu3.hu3chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hu3chatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
